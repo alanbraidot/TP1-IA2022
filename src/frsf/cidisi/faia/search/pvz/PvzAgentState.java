7e4f0e7f4b2ce8ee2734ffb4cf6607c6f994814d
alanbraidot <alan.br.1998@gmail.com>
2022-04-15 18:05:52 -0300

    Estructura inicial

--- src/frsf/cidisi/faia/search/pvz/PvzAgentState.java
+++ src/frsf/cidisi/faia/search/pvz/PvzAgentState.java
@@ -2,6 +2,33 @@ package frsf.cidisi.faia.search.pvz;
 
 public class PvzAgentState extends SearchBasedAgentState {
 
-    public PvzAgentState(int )
+    private int[] position;
+    private int suns;
+    private int zombies;
+    private int[][] garden;
+    private int[] initialPosition;
+
+
+    public PvzAgentState(int row, int col, int suns, int zombies, int[][] g){
+        this.garden = g;
+        this.position = new int[] {row, col};
+        this.initialPosition = new int[2];
+        this.initialPosition[0] = row;
+        this.initialPosition[1] = col;
+        this.suns = suns;
+        this.zombies = zombies;
+    }
+
+    public PvzAgentState(){
+        this.garden = new int[5][9];
+        position = new int[2];
+        this.initState();
+    }
+
+    @Override
+    public void initState(){
+        
+    }
+
     
 }
