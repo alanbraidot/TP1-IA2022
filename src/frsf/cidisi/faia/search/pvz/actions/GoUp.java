945ac450536399648feefa74d965f658e5070674
alanbraidot <alan.br.1998@gmail.com>
2022-04-16 16:58:49 -0300

    Implementacion parcial GoUp()

--- src/frsf/cidisi/faia/search/pvz/actions/GoUp.java
+++ src/frsf/cidisi/faia/search/pvz/actions/GoUp.java
@@ -2,6 +2,7 @@ package frsf.cidisi.faia.search.pvz.actions;
 
 import frsf.cidisi.faia.agent.search.SearchAction;
 import frsf.cidisi.faia.agent.search.SearchBasedAgentState;
+import frsf.cidisi.faia.search.pvz.PvzAgentState;
 import frsf.cidisi.faia.state.AgentState;
 import frsf.cidisi.faia.state.EnvironmentState;
 
@@ -10,13 +11,24 @@ public class GoUp extends SearchAction{
 	@Override
 	public SearchBasedAgentState execute(SearchBasedAgentState s) {
 		// TODO Auto-generated method stub
-		return null;
+		PvzAgentState pvzState = (PvzAgentState) s;
+		
+		int row = pvzState.getRowPosition();
+		int col = pvzState.getColumnPosition();
+		
+		if(row < 4)
+				row = row + 1;
+		else
+			//Throw exception
+			
+		pvzState.setRowPosition(row);
+		
+		return pvzState;
 	}
 
 	@Override
 	public Double getCost() {
-		// TODO Auto-generated method stub
-		return null;
+		return 0.0;
 	}
 
 	@Override
@@ -27,8 +39,7 @@ public class GoUp extends SearchAction{
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		return "GoUp";
 	}
     
 }
